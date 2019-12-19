8229f9ca7f7bd445bf1a734bbda1573b03e5564f
keshavkhatri <keshav.khatri@ranosys.com>
2019-12-19 16:41:40 +0530

    sidebar and theme setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    ListComponent,
+    DetailComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
