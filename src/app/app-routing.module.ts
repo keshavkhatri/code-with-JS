6b1e123e543b1e06b8882389bc807e9a4700d2c1
keshavkhatri <keshav.khatri@ranosys.com>
2019-12-20 14:25:03 +0530

    added basic page design with dummy content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'list',
+        component: ListComponent
+    },
+    {
+        path: 'blog/:id',
+        component: DetailComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
