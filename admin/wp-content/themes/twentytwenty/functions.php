53a355ac3219d79a98067b2bb059b76dd2be4cda
Keshav Khatri <keshu88khatri@gmail.com>
2020-08-07 18:43:11 +0530

    WIP

--- admin/wp-content/themes/twentytwenty/functions.php
+++ admin/wp-content/themes/twentytwenty/functions.php
@@ -812,3 +812,8 @@ add_action('rest_api_init', function () {
         'callback' => 'customContactForm',
     ));
 });
+
+function add_cors_http_header(){
+    header("Access-Control-Allow-Origin: *");
+}
+add_action('init','add_cors_http_header');
