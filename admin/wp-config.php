12c71c9b42a4c5e2999309737d0167e8453d0cea
Keshav Khatri <keshu88khatri@gmail.com>
2020-08-07 17:54:42 +0530

    WIP

--- admin/wp-config.php
+++ admin/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'code-with-js' );
+define( 'DB_NAME', 'codewithjs' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'codewithjs' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Ranosys@123' );
+define( 'DB_PASSWORD', 'hfkaysd&6s0' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
--- src/app/core/system.constants.ts
+++ src/app/core/system.constants.ts
@@ -1,5 +1,5 @@
 export const SYSTEM_CONSTANTS = {
-    API_ROOT: 'http://localhost/code-with-JS/admin/wp-json/wp/',
+    API_ROOT: 'http://www.codewithjs.com/admin/wp-json/wp/posts',
     API_VERSION: 'V2/',
     DEFAULTPAGE: 1,
     DEFAULTPAGESIZE: 10,
