b13508361d63c87ec04b7888f4d1b73648560669
Keshav Khatri <keshu88khatri@gmail.com>
2020-08-10 16:56:14 +0530

    WIP

--- admin/wp-content/themes/twentytwenty/functions.php
+++ admin/wp-content/themes/twentytwenty/functions.php
@@ -840,7 +840,7 @@ function initCors($value)
 	header('Access-Control-Allow-Origin: ' . $origin_url);
 	header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 	header('Access-Control-Allow-Credentials: true');
-	header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+	header('Access-Control-Allow-Headers: cache-control, Cache-Control, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 	return $value;
 }
 add_action('rest_api_init', function () {
